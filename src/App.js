081fe82db60fd8758fcd8d5c7d9cf5a32db78973
SonnySmothers <sonny.smothers@g.fmarion.edu>
2025-04-25 13:32:54 -0400

    Fixed Return DVD still present and causing errors

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Inventory from './Components/Inventory/Inventory.jsx';
 import ManageAlerts from './Components/ManageAlerts/ManageAlerts.jsx';
 import ManageEmployees from './Components/ManageEmployees/ManageEmployees.jsx';
 import RentSellDVD from './Components/RentSellDVD/RentSellDVD.jsx';
-import ReturnDVD from './Components/ReturnDVD/ReturnDVD.jsx';
 import TrackRental from './Components/TrackRental/TrackRental.jsx';
 import { NavigationManager, useMyContext } from './Components/NavigationManager/NavigationManager.jsx';
 
@@ -24,7 +23,6 @@ function App() {
         <Route path="/manage-alerts" element={<ManageAlerts />} />
         <Route path="/manage-employees" element={<ManageEmployees />} />
         <Route path="/rent-sell-dvd" element={<RentSellDVD />} />
-        <Route path="/return-dvd" element={<ReturnDVD />} />
         <Route path="/track-rental" element={<TrackRental />} />
         <Route path="*" element={<Navigate to="/login" />} /> {/* Redirect unknown routes */}
       </Routes>
